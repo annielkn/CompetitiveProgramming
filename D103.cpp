eaa8b0609e33d324f3cbbf2b81f5a2ba42b8fe5c
annielkn <61274383+annielkn@users.noreply.github.com>
2020-02-27 00:30:06 +0800

    Create D103.cpp

---
+++ D103.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio> 
+#define PI 3.14159265
+using namespace std;
+
+int main() {
+	double ret = 0.00, grad = 0.00;
+	int a = 0, b = 0, c = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	grad = c * PI / 180;
+	ret = sin(grad);
+	ret = ret * a * b;
+	printf("%.3lf ", ret/2);
+	return 0;
+}
