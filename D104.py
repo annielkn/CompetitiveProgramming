c0cfbcc39d909c13f1106a2417173fea5d4724db
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-02 00:20:41 +0800

    Update D104.py

--- D104.py
+++ D104.py
@@ -1,14 +1,14 @@
-import math
+from math import sqrt
 y = 0
 numbers = [int(n) for n in input().split()]
 x = numbers[1]*numbers[1]-4*numbers[0]*numbers[2]
 if x < 0:
 	print ("None")
 elif x == 0:
-	y = (numbers[1]*(-1)+math.sqrt(x))/(2*numbers[0])
+	y = (numbers[1]*(-1)+sqrt(x))/(2*numbers[0])
 	print ("%.3f" % round(y,2))
 elif x > 0:
-	y = (numbers[1]*(-1)+math.sqrt(x))/(2*numbers[0])
+	y = (numbers[1]*(-1)+sqrt(x))/(2*numbers[0])
 	print ("%.3f" % round(y,2))
-	y = (numbers[1]*(-1)-math.sqrt(x))/(2*numbers[0])
+	y = (numbers[1]*(-1)-sqrt(x))/(2*numbers[0])
 	print ("%.3f" % round(y,2))
