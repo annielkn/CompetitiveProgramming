0c590f5f84ff05ffa21f9501bdb6b4a15f5605db
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-03 00:30:55 +0800

    Update D203.py

--- D203.py
+++ D203.py
@@ -3,10 +3,13 @@ x = 1
 N = int(input(""))
 while x < 101:
 	if (x % N == 0):
-		print ("Clap", end = " ")
+		if (x % 10 == 0):
+			print ("Clap", end = "\n")
+		else:
+			print ("Clap", end = " ")
 	elif (x % 10 == N):
 		print ("Clap", end = " ")
-	elif (x - N * 10 > 0 and x - 70 < 10):
+	elif (x - N * 10 > 0 and x - N * 10 < 10):
 		print ("Clap", end = " ")
 	elif (x % 10 == 0):
 		print (x, end = "\n")
