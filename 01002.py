9628b31737aa984069952d334b1c67772033cc03
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-04 00:12:23 +0800

    Update 01002.py

--- 01002.py
+++ 01002.py
@@ -1,13 +1,13 @@
 string = input("")
 target = input("")
-n = -1
+n = 0
 r = 0
 i = 0
 while (r!=-1):
-	r = string.find(target, n+1)
+	r = string.find(target, n)
 	i+=1
 	print(r)
 	if (r != -1):
-		n = r
+		n = r + 1
 		
 print (i-1)
