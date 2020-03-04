d98bbd1eeff14cd437ce111379c7365a131e0829
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-05 00:17:23 +0800

    Update J024.py

--- J024.py
+++ J024.py
@@ -2,8 +2,8 @@ import statistics
 n = int(input())
 arr = [ int(input()) for i in range(n)]
 print("%.3f" % statistics.mean(arr))
-if isinstance(statistics.median(arr), int):
-	print(statistics.median(arr))
+if statistics.median(arr) == int(statistics.median(arr)):
+	print(int(statistics.median(arr)))
 else:
 	print("%.1f" % statistics.median(arr))
 print(statistics.mode(arr))
