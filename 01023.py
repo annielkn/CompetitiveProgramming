0f80bee656b5733c0bfcfe4d6c47493a9e39bb08
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-07 20:27:42 +0800

    Create 01023.py

---
+++ 01023.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+N = arr[0]
+K = arr[1]
+temp = list(map(int, input().split()))
+mini = abs(temp[0]-K)
+minikey = temp[0]
+for i in range(len(temp)):
+	x = abs(temp[i]-K)
+	if (x<mini):
+		mini = x
+		minikey = temp[i]
+print(minikey)
