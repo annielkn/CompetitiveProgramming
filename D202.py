a853ce1e145e098cc9e7383be0e89cb1168dd014
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-09 00:41:39 +0800

    Update D202.py

--- D202.py
+++ D202.py
@@ -1,6 +1,14 @@
-n = int(input(""))
-i = 0
-while i <= n:
-	i+=1
-	if (n % i == 0):
-		print (i)
+from math import sqrt
+x = int(input())
+factors = []
+i = 1
+while i <= sqrt(x):
+	if i == sqrt(x):
+		factors.append(i)
+	elif x % i == 0:
+		factors.append(i)
+		factors.append(int(x/i))
+	i += 1
+factors = sorted(factors)
+for n in range(len(factors)):
+	print (factors[n])
