d04699bafd02a1e4f91a47287b2974088eba1619
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-09 00:41:12 +0800

    Update D104.py

--- D104.py
+++ D104.py
@@ -6,9 +6,15 @@ if x < 0:
 	print ("None")
 elif x == 0:
 	y = (numbers[1]*(-1)+sqrt(x))/(2*numbers[0])
-	print ("%.3f" % round(y,2))
+	print ("%.3f" % round(y,3))
 elif x > 0:
-	y = (numbers[1]*(-1)+sqrt(x))/(2*numbers[0])
-	print ("%.3f" % round(y,2))
-	y = (numbers[1]*(-1)-sqrt(x))/(2*numbers[0])
-	print ("%.3f" % round(y,2))
+	a = (numbers[1]*(-1)+sqrt(x))/(2*numbers[0])
+	#print ("%.3f" % round(y,2), end = " ")
+	b = (numbers[1]*(-1)-sqrt(x))/(2*numbers[0])
+	if (a<b):
+		print ("%.3f" % round(a,3), end = " ")
+		print ("%.3f" % round(b,3))
+	elif (a>b):
+		print ("%.3f" % round(b,3), end = " ")
+		print ("%.3f" % round(a,3))
+	#print ("%.3f" % round(y,2))
