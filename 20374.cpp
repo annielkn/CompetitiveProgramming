710caf6bc82a4241a90fc2750d897e812e0ac74c
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-09 00:42:40 +0800

    Update 20374.cpp

--- 20374.cpp
+++ 20374.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 using namespace std;
 
 int main() {
 	long long p = 0; long long b = 0; long long m = 0; long long r = 0;
 	scanf("%lld\n%lld\n%lld\n", &b, &p, &m);
-	r = ((b % m)^p) % m;
+	r = pow((b % m), p);
+	r  = r % m;
 	printf("%lld", r);
 	return 0;
 }
