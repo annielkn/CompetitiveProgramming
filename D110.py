e17334a1e50c9f557f93715271334bdd2625d0fa
annielkn <61274383+annielkn@users.noreply.github.com>
2020-03-10 00:26:59 +0800

    Update D110.py

--- D110.py
+++ D110.py
@@ -3,7 +3,18 @@ from math import ceil
 temp = input()
 x1 = ord(temp[0])-96
 y1 = int(temp[1])
+#print (x1, y1)
 temp = input()
 x2 = ord(temp[0])-96
 y2 = int(temp[1])
-print(ceil(sqrt((y2-y1)**2+(x2-x1)**2))-1)
+a = abs(x1-x2)
+b = abs(y1-y2)
+
+cnt = 0
+cnt = min(a, b)
+n = cnt
+#print (a, b, cnt)
+if a == b: 
+	print(a)
+else: 
+	print(cnt+abs(n-a)+abs(n-b))
